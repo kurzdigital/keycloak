82b0437189adcf8992844acc8ce4fa74caa48587
Nikolai Prokoschenko <nikolai.prokoschenko@kurzdigital.com>
2017-03-17 09:25:44 +0100

    HTTPS

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -4,5 +4,19 @@ if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
     keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 fi
 
+if [ "$KEYCLOAK_REVERSE_PROXY" = "true" ]; then
+    "$JBOSS_HOME/bin/jboss-cli.sh" << EOF
+embed-server --server-config=standalone.xml
+batch
+/socket-binding-group=standard-sockets/socket-binding=proxy-https:add(port=443)
+/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=proxy-address-forwarding,value=true)
+/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=redirect-socket,value=proxy-https)
+run-batch
+stop-embedded-server
+EOF
+
+fi
+
 exec /opt/jboss/keycloak/bin/standalone.sh $@
+
 exit $?
