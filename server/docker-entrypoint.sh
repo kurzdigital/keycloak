593d41ef7e640a763db20d4f65953bb17e9f4800
Nikolai Prokoschenko <nikolai.prokoschenko@kurzdigital.com>
2017-03-17 09:51:20 +0100

    add HA

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -13,6 +13,13 @@ batch
 /subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=redirect-socket,value=proxy-https)
 run-batch
 stop-embedded-server
+embed-server --server-config=standalone-ha.xml
+batch
+/socket-binding-group=standard-sockets/socket-binding=proxy-https:add(port=443)
+/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=proxy-address-forwarding,value=true)
+/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=redirect-socket,value=proxy-https)
+run-batch
+stop-embedded-server
 EOF
 
 fi
